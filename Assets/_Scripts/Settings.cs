95bc26e55770df9a06b934933109ea85f847f190
Maxim Korovin <fazamkg@gmail.com>
2025-07-08 22:19:43 +0500

    Add lefty setting

--- Assets/_Scripts/Settings.cs
+++ Assets/_Scripts/Settings.cs
@@ -1,11 +1,19 @@
+using System;
+
 namespace Faza
 {
     public static class Settings
     {
+        public static event Action OnIsLeftyChanged;
+
         public static bool IsLefty
         {
             get => Storage.GetBool("faza_is_lefty");
-            set => Storage.SetBool("faza_is_lefty", value);
+            set
+            {
+                Storage.SetBool("faza_is_lefty", value);
+                OnIsLeftyChanged?.Invoke();
+            }
         }
     } 
 }
--- Assets/_Scripts/UI/UserInterface.cs
+++ Assets/_Scripts/UI/UserInterface.cs
@@ -7,10 +7,42 @@ namespace Faza
     {
         [SerializeField] private MyButton _settingsButton;
         [SerializeField] private SettingsPanel _settingsPanel;
+        [SerializeField] private RectTransform _leftJoystick;
+        [SerializeField] private RectTransform _rightJoystick;
 
         private void Awake()
         {
+            Settings.OnIsLeftyChanged += Settings_OnIsLeftyChanged;
+
             _settingsButton.OnUp += SettingsButton_OnUp;
+
+            var isLefty = Settings.IsLefty;
+            if (isLefty)
+            {
+                _leftJoystick.localScale = Vector3.one;
+                _rightJoystick.localScale = Vector3.zero;
+            }
+            else
+            {
+                _leftJoystick.localScale = Vector3.zero;
+                _rightJoystick.localScale = Vector3.one;
+            }
+        }
+
+        private void Settings_OnIsLeftyChanged()
+        {
+            var isLefty = Settings.IsLefty;
+
+            if (isLefty)
+            {
+                _leftJoystick.DOScale(1f, 0.3f).SetEase(Ease.OutBack);
+                _rightJoystick.DOScale(0f, 0.3f).SetEase(Ease.InBack);
+            }
+            else
+            {
+                _leftJoystick.DOScale(0f, 0.3f).SetEase(Ease.InBack);
+                _rightJoystick.DOScale(1f, 0.3f).SetEase(Ease.OutBack);
+            }
         }
 
         private void SettingsButton_OnUp()
