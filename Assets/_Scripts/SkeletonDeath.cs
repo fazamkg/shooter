c625a5a268b9f7217339517feeaa823fb31c3d2e
Maxim Korovin <fazamkg@gmail.com>
2025-07-11 22:22:31 +0500

    Optimize mesh splitter

--- Assets/_Scripts/SkeletonDeath.cs
+++ Assets/_Scripts/SkeletonDeath.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
 using DG.Tweening;
@@ -10,10 +9,8 @@ namespace Faza
         [SerializeField] private Health _health;
         [SerializeField] private SkinnedMeshRenderer _skin;
         [SerializeField] private Coin _coinPrefab;
-        [SerializeField] private AnimationCurve _coinVerticalCurve;
-        [SerializeField] private AnimationCurve _coinHorizontalCurve;
-
-        private List<Rigidbody> _created = new();
+        [SerializeField] private GameObject _gibHolder;
+        [SerializeField] private Rigidbody[] _gibs;
 
         private void Awake()
         {
@@ -22,11 +19,10 @@ namespace Faza
 
         private void Health_OnDeath()
         {
-            var baked = new Mesh();
-            _skin.BakeMesh(baked);
-            SplitMeshIntoIslands(baked);
-
+            _gibHolder.SetActive(true);
             _skin.enabled = false;
+
+            ThrowGibs();
 
             StartCoroutine(DisableGibs());
         }
@@ -34,192 +30,25 @@ namespace Faza
         private IEnumerator DisableGibs()
         {
             yield return new WaitForSeconds(10f);
-            foreach (var thing in _created)
+            foreach (var thing in _gibs)
             {
                 thing.constraints = RigidbodyConstraints.FreezeAll;
                 thing.detectCollisions = false;
             }
         }
 
-        private void SplitMeshIntoIslands(Mesh mesh)
+        private void ThrowGibs()
         {
-            var verts = mesh.vertices;
-            var tris = mesh.triangles;
-            var uvs = mesh.uv;
-            var normals = mesh.normals;
-
-            var triCount = tris.Length / 3;
-
-            var uniquePositions = new List<Vector3>();
-            var positionMap = new int[verts.Length]; // original vert index => unique position index
-
-            for (var i = 0; i < verts.Length; i++)
+            foreach (var gib in _gibs)
             {
-                var current = verts[i];
-                var found = false;
-
-                for (int j = 0; j < uniquePositions.Count; j++)
-                {
-                    if (Vector3.Distance(uniquePositions[j], current) < 0.001f)
-                    {
-                        positionMap[i] = j;
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (found == false)
-                {
-                    positionMap[i] = uniquePositions.Count;
-                    uniquePositions.Add(current);
-                }
-            }
-
-            // key = index of vertex
-            // value = list of triangle indexes
-            var vertToTri = new Dictionary<int, List<int>>();
-            // adjacency of vertex to triangle
-
-            for (var i = 0; i < triCount; i++)
-            {
-                var i0 = positionMap[tris[i * 3]];
-                var i1 = positionMap[tris[i * 3 + 1]];
-                var i2 = positionMap[tris[i * 3 + 2]];
-
-                if (vertToTri.ContainsKey(i0) == false) vertToTri[i0] = new List<int>();
-                if (vertToTri.ContainsKey(i1) == false) vertToTri[i1] = new List<int>();
-                if (vertToTri.ContainsKey(i2) == false) vertToTri[i2] = new List<int>();
-
-                // i = triangle index
-
-                vertToTri[i0].Add(i);
-                vertToTri[i1].Add(i);
-                vertToTri[i2].Add(i);
-            }
-
-            var triToTri = new List<int>[triCount];
-            // adjacency of triangle to triangle
-            for (var i = 0; i < triCount; i++)
-            {
-                triToTri[i] = new List<int>();
-
-                var i0 = positionMap[tris[i * 3]];
-                var i1 = positionMap[tris[i * 3 + 1]];
-                var i2 = positionMap[tris[i * 3 + 2]];
-
-                var neighbors = new HashSet<int>();
-
-                foreach (var t in vertToTri[i0]) neighbors.Add(t);
-                foreach (var t in vertToTri[i1]) neighbors.Add(t);
-                foreach (var t in vertToTri[i2]) neighbors.Add(t);
-
-                neighbors.Remove(i);
-
-                triToTri[i].AddRange(neighbors);
-            }
-
-            var visited = new bool[triCount];
-            var islands = new List<List<int>>();
-
-            for (var i = 0; i < triCount; i++)
-            {
-                if (visited[i]) continue;
-
-                var stack = new Stack<int>();
-                var islandTris = new List<int>();
-
-                stack.Push(i);
-                visited[i] = true;
-
-                while (stack.Count > 0)
-                {
-                    var current = stack.Pop();
-                    islandTris.Add(current);
-
-                    foreach (var neighbor in triToTri[current])
-                    {
-                        if (visited[neighbor] == false)
-                        {
-                            visited[neighbor] = true;
-                            stack.Push(neighbor);
-                        }
-                    }
-                }
-
-                islands.Add(islandTris);
-            }
-
-            for (var i = 0; i < islands.Count; i++)
-            {
-                var triIndexes = islands[i];
-
-                var newVerts = new List<Vector3>();
-                var newTris = new List<int>();
-                var newUvs = new List<Vector2>();
-                var newNormals = new List<Vector3>();
-                var vertexMap = new Dictionary<int, int>();
-
-                foreach (var triIndex in triIndexes)
-                {
-                    int[] oldTri =
-                    {
-                        tris[triIndex * 3],
-                        tris[triIndex * 3 + 1],
-                        tris[triIndex * 3 + 2],
-                    };
-
-                    for (int j = 0; j < 3; j++)
-                    {
-                        var oldVertex = oldTri[j];
-                        if (vertexMap.ContainsKey(oldVertex) == false)
-                        {
-                            vertexMap[oldVertex] = newVerts.Count;
-                            newVerts.Add(verts[oldVertex]);
-                            newUvs.Add(uvs[oldVertex]);
-                            newNormals.Add(normals[oldVertex]);
-                        }
-                        newTris.Add(vertexMap[oldVertex]);
-                    }
-                }
-
-                var newMesh = new Mesh();
-                newMesh.vertices = newVerts.ToArray();
-                newMesh.triangles = newTris.ToArray();
-                newMesh.uv = newUvs.ToArray();
-                newMesh.normals = newNormals.ToArray();
-                newMesh.RecalculateBounds();
-
-                var go = new GameObject("island");
-                var meshFilter = go.AddComponent<MeshFilter>();
-                var meshRenderer = go.AddComponent<MeshRenderer>();
-
-                go.layer = LayerMask.NameToLayer("Gibs");
-
-                meshFilter.sharedMesh = newMesh;
-                meshRenderer.sharedMaterial = _skin.sharedMaterial;
-
-                go.transform.position = _skin.transform.position;
-                go.transform.rotation = _skin.transform.rotation;
-                go.transform.localScale = _skin.transform.localScale;
-
-                var rigidbody = go.AddComponent<Rigidbody>();
-                rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
-                rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
-                rigidbody.angularDrag = 0f;
-
-                var meshCollider = go.AddComponent<BoxCollider>();
-                meshCollider.sharedMaterial = Resources.Load<PhysicMaterial>("Gib");
-
                 var horizontalSpeed = Random.Range(3f, 5f);
                 var verticalSpeed = Random.Range(10f, 14f);
 
                 var angle = Random.Range(-10f, 10f);
                 var direction = Quaternion.Euler(0f, angle, 0f) * _health.LastDamageDirection;
-                rigidbody.AddForce(direction * horizontalSpeed, ForceMode.VelocityChange);
-                rigidbody.AddForce(Vector3.up * verticalSpeed, ForceMode.VelocityChange);
-                rigidbody.AddTorque(Random.onUnitSphere * 1000f, ForceMode.VelocityChange);
-
-                _created.Add(rigidbody);
+                gib.AddForce(direction * horizontalSpeed, ForceMode.VelocityChange);
+                gib.AddForce(Vector3.up * verticalSpeed, ForceMode.VelocityChange);
+                gib.AddTorque(Random.onUnitSphere * 1000f, ForceMode.VelocityChange);
             }
 
             for (var i = 0; i < 3; i++)
