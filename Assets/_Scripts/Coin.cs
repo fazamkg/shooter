ee82571d213b8ec23bdb0350e1348db77f5d94f7
Maxim Korovin <fazamkg@gmail.com>
2025-07-12 19:30:25 +0500

    Make coins fly-in into player

--- Assets/_Scripts/Coin.cs
+++ Assets/_Scripts/Coin.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 namespace Faza
 {
@@ -8,6 +9,8 @@ namespace Faza
         [SerializeField] private float _speed;
 
         private Rigidbody _target;
+        private bool _picked;
+        private PlayerInput _player;
 
         public void SetTarget(Rigidbody rb)
         {
@@ -16,19 +19,39 @@ namespace Faza
 
         private void Update()
         {
-            transform.position = _target.worldCenterOfMass + Vector3.up;
+            if (_picked == false)
+            {
+                transform.position = _target.worldCenterOfMass + Vector3.up;
+            }
+            else
+            {
+                var target = _player.transform.position.DeltaY(1f);
+                var dist = Vector3.Distance(transform.position, target);
+                var speed = dist * Time.deltaTime * 8f;
 
+                if (dist < 0.15f)
+                {
+                    Currency.Coins++;
+                    gameObject.SetActive(false);
+                }
+
+                transform.position = Vector3.MoveTowards(transform.position, target, speed);
+
+                transform.localScale = Vector3.one * Mathf.Min(dist * 2f, 1f);
+            }
+            
             _transform.Rotate(0f, Time.deltaTime * _speed, 0f, Space.World);
         }
 
         private void OnTriggerEnter(Collider other)
         {
+            if (_picked) return;
+
             var player = other.GetComponent<PlayerInput>();
             if (player == false) return;
 
-            Currency.Coins++;
-
-            gameObject.SetActive(false);
+            _picked = true;
+            _player = player;
         }
     } 
 }
