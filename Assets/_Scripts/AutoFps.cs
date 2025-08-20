6eed09b4e05431c3cc9001c5743ed2bc281b6181
Maxim Korovin <fazamkg@gmail.com>
2025-08-20 20:45:22 +0500

    Add auto fps

---
+++ Assets/_Scripts/AutoFps.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Faza
+{
+    public class AutoFps : MonoBehaviour
+    {
+        [SerializeField] private UniversalRenderPipelineAsset _settings;
+
+        private int _frames;
+
+        private void Awake()
+        {
+            StartCoroutine(Adjust());
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Update()
+        {
+            _frames++;
+        }
+
+        private IEnumerator Adjust()
+        {
+            yield return new WaitForSecondsRealtime(5f);
+
+            var oldFps = _frames / 5f;
+
+            if (oldFps >= 58f)
+            {
+                print("faza: fps is decent");
+                yield break;
+            }
+
+            while (true)
+            {
+                _frames = 0;
+                _settings.renderScale -= 0.1f;
+                print("faza: decrease resolution");
+
+                yield return new WaitForSecondsRealtime(5f);
+
+                var newFps = _frames / 5f;
+
+                if ((newFps - oldFps).Abs() < 5f) 
+                {
+                    print("faza: fps is capped or we are cpu bound");
+                    // most likely cpu bound here OR frame capped (vsync or browser or smth)
+                    // so let's stop decreasing resolution
+
+                    _settings.renderScale += 0.1f;
+                    yield break;
+                }
+
+                if (newFps >= 58f)
+                {
+                    print("faza: fps is decent");
+                    yield break;
+                }
+
+                oldFps = newFps;
+            }
+        }
+    } 
+}
