325383d97df63e3dd6e8546c7b8a77178cba6be3
Maxim Korovin <fazamkg@gmail.com>
2025-10-18 16:49:48 +0500

    Remove unused usings

--- Assets/_Scripts/EntryPoint.cs
+++ Assets/_Scripts/EntryPoint.cs
@@ -1,8 +1,5 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
-using System;
-using YG;
 
 namespace Faza
 {
--- Assets/_Scripts/_Core/Storage.cs
+++ Assets/_Scripts/_Core/Storage.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Globalization;
-using UnityEngine;
 using YG;
 
 namespace Faza
